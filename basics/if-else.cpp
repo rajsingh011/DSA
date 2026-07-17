4fa9129e41fdba8fb0c599a6f33f75fce713e14a
rajsingh011 <trpdpqb5n5@privaterelay.appleid.com>
2026-07-17 16:57:50 +0530

    solved: if-else - basics

---
+++ basics/if-else.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    void studentGrade(int marks) {
+if (marks>=90){
+    cout<<"Grade A"<<endl;
+}
+else if (marks>=70){
+    cout<<"Grade B"<<endl;
+}
+else if (marks>=50){
+    cout<<"Grade C"<<endl;
+}
+else if (marks >=35){
+    cout<<"Grade D"<<endl;
+}
+else {
+    cout<<"Fail"<<endl;
+}
+    }
+};
+int main (){
+    Solution obj;
+    obj.studentGrade(90);
+    return 0;
+}
