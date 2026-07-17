d332a310c98bafed2c2d54522750adcf834b4ab3
rajsingh011 <trpdpqb5n5@privaterelay.appleid.com>
2026-07-17 16:57:50 +0530

    solved: input output - basics

---
+++ basics/input-output.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    void printNumber() {
+        int n;
+        cin >> n;
+        cout << n;
+    }
+};
+
+int main() {
+    Solution obj;
+    obj.printNumber();
+    return 0;
+}
