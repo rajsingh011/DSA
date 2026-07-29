00d4ac2c5d7ff49ea0a2488b111210722eec3ba2
rajsingh011 <trpdpqb5n5@privaterelay.appleid.com>
2026-07-29 15:19:11 +0530

    Added pattern14.cpp

---
+++ Patterns/pattern14.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void pattern14(int n) {
+for (int i=0;i<n;i++){
+    for (char ch = 'A'; ch <='A'+i;ch++){
+        cout << ch << " ";
+    }
+    
+    cout << endl;
+}
+}
+    };
+int main (){
+    char n;
+    n= 5;
+    Solution obj;
+    obj.pattern14(n);
+    return 0;
+}
